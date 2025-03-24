af5cfb9baf300d003fdce3b86d8c02a351e6051c
WhitesLove <kubai.rita5@gmail.com>
2025-03-25 00:38:46 +0300

    Fix init nickname

--- tabs/common/init.js
+++ tabs/common/init.js
@@ -1,4 +1,3 @@
-import {saveSettings} from "../../utils/utils.js";
 import {loadCharacters, renderCharacters} from "../characters/characters.js";
 
 export default async function (nicknameElement, editNicknameButton, saveNicknameButton) {
@@ -10,28 +9,18 @@ export default async function (nicknameElement, editNicknameButton, saveNickname
         saveNicknameButton.style.display = 'inline-block';
     } else {
         nicknameElement.innerText = `Ваш ник: ${nickname}`;
-    }
 
-    try {
-        const allNickCharacters = window.settings.allNickCharacters || {};
-
-        if (!allNickCharacters[nickname]) {
-            const characters = await window.electron.ipcRenderer.fetchCharacters(nickname);
-            if (characters && !characters?.error) {
-                allNickCharacters[nickname] = characters;
-                saveSettings({allNickCharacters: allNickCharacters});
+        try {
+            if (window.settings.characterList) {
+                renderCharacters(false);
+            } else {
+                await loadCharacters(nickname);
             }
+        } catch (error) {
+            console.error("Ошибка загрузки ника:", error);
+            nicknameElement.innerText = "Ошибка загрузки ника";
+            editNicknameButton.style.display = 'block';
+            saveNicknameButton.style.display = 'none';
         }
-
-        if (window.settings.characterList) {
-            renderCharacters(false);
-        } else {
-            await loadCharacters(nickname);
-        }
-    } catch (error) {
-        console.error("Ошибка загрузки ника:", error);
-        nicknameElement.innerText = "Ошибка загрузки ника";
-        editNicknameButton.style.display = 'block';
-        saveNicknameButton.style.display = 'none';
     }
 }
