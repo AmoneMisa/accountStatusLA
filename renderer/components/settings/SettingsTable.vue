27bee39af4d4af1b87671b476024cfb3c8696b8a
WhitesLove <kubai.rita5@gmail.com>
2025-05-03 19:37:07 +0300

    Fix single update character; Added sticky position for anchors in tools;

--- renderer/components/settings/SettingsTable.vue
+++ renderer/components/settings/SettingsTable.vue
@@ -267,7 +267,7 @@ async function generateLogAndOpenFolder() {
 
     <div class="settings-table__cell">Проверить обновления приложения</div>
     <div class="settings-table__cell">
-      <button type="button" id="update-app" class="button" data-current-version="0.17" @click="updateApp">
+      <button type="button" id="update-app" class="button" data-current-version="0.17.1" @click="updateApp">
         Обновить приложение
       </button>
     </div>
