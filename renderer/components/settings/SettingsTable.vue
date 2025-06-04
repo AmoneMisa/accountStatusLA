89a765a2d3c165dd7640d5d5e5c809c66a2183a0
WhitesLove <kubai.rita5@gmail.com>
2025-06-04 22:17:30 +0300

    Update raid gold

--- renderer/components/settings/SettingsTable.vue
+++ renderer/components/settings/SettingsTable.vue
@@ -339,7 +339,7 @@ function changeTabVisibility(modelKey, target) {
 
     <div class="settings-table__cell">Проверить обновления приложения</div>
     <div class="settings-table__cell">
-      <button type="button" id="update-app" class="button" data-current-version="2.3.0" @click="updateApp">
+      <button type="button" id="update-app" class="button" data-current-version="2.3.1" @click="updateApp">
         Обновить приложение
       </button>
     </div>
